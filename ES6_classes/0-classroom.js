74a9e669cb9bfcdfe31b41ae7a00090e054ad294
Josephine180 <pierrejosephine09@gmail.com>
2025-03-26 10:04:30 +0100

    add _

--- ES6_classes/0-classroom.js
+++ ES6_classes/0-classroom.js
@@ -1,7 +1,5 @@
 export default class ClassRoom {
-  // constructeur avec l'argument
   constructor(maxStudensSize) {
-    this._maxStrudentsSize = maxStudensSize;
-    // Assigner l'argument à la propriété
+    this._maxStudentsSize = maxStudensSize;
   }
 }
