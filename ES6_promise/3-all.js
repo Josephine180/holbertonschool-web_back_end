30680da0e2cd66b1744d9da96382de6e86f8f961
Josephine180 <pierrejosephine09@gmail.com>
2025-04-01 13:21:02 +0200

    add £

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -3,7 +3,7 @@ import { uploadPhoto, createUser } from './utils';
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
     .then(([Photo, User]) => {
-      console.log(Photo.body, User.firstName, User.lastName);
+      console.log(`${Photo.body} ${User.firstName} ${User.lastName}`);
     })
     .catch(() => {
       console.log('Signup system offline');
