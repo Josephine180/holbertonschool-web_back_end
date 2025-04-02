b9a5d90bff0fb0249b130629b864001df3be779e
Josephine180 <pierrejosephine09@gmail.com>
2025-04-02 09:43:59 +0200

    add promise.reject

--- ES6_promise/5-photo-reject.js
+++ ES6_promise/5-photo-reject.js
@@ -1,5 +1,4 @@
 export default function uploadPhoto(filename) {
-  return new Promise((reject) => {
-    reject(new Error(`${filename} cannot be processed`));
-  });
+  const Firstpromise = Promise.reject(new Error(`${filename} cannot be processed`));
+  return Firstpromise;
 }
---
+++ ES6_promise/6-final-user.js
@@ -0,0 +1,4 @@
+import {signeUpUser} from './4-user-promise';
+import { uploadPhoto } from './utils';
+
+export default function handleProfileSignup()
