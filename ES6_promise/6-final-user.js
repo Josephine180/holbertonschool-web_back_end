de016207958646f74147dd20ddbedd7abc5184e6
Josephine180 <pierrejosephine09@gmail.com>
2025-04-02 10:37:19 +0200

    add handle multiple promises

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,4 +1,12 @@
-import {signeUpUser} from './4-user-promise';
-import { uploadPhoto } from './utils';
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup()
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ]).then((results) => results.map(({ status, value, reason }) => ({
+    status,
+    value: value || reason,
+  })));
+}
