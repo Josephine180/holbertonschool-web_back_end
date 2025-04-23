d2bbbe43049d4e93c31a09ac0546d3becce9f2e4
Josephine180 <pierrejosephine09@gmail.com>
2025-04-23 10:53:27 +0200

    add file and type annotation function add

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+def add(a: float, b: float) -> float:
+    """
+        Return sum of two floats
+        This function takes two float arguments and returns their addition
+        as a float.
+    """
+    return a + b
