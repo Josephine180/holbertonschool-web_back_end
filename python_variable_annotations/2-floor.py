054b4ad7930d75912701ebe558657e6b364c23ce
Josephine180 <pierrejosephine09@gmail.com>
2025-04-23 11:09:14 +0200

    add floar type int

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+
+"""
+    This module is a function that takes a float as argument
+"""
+
+
+def floor(n: float) -> int:
+    """
+    This function takes a float n as argument.
+    It returns the floor of the float.
+    """
+    return int(n)
