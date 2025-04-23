a621ae687196f9cc249a380317aca10bf51b7f02
Josephine180 <pierrejosephine09@gmail.com>
2025-04-23 11:14:19 +0200

    add function that turns a float in a string

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+"""
+    This module is a function str that takes a float.
+
+"""
+
+
+def to_str(n: float) -> str:
+    """
+        function with n a float as argument.
+        Returns a string.
+    """
+    return str(n)
