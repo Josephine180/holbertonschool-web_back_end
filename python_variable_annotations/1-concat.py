d0482a2de1c31c5f479f3fb6619131b25887471a
Josephine180 <pierrejosephine09@gmail.com>
2025-04-23 11:00:50 +0200

    add concatenation of strings

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+
+"""
+    This module is a concatenation of two strings.
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+        The function takes 2 parameter str1 and str2.
+        It returns a concatenation of strings.
+    """
+    return str1 + str2
