959b25de5c63d4f978dd283732455d2e67d1f6db
Josephine180 <pierrejosephine09@gmail.com>
2025-04-23 12:20:07 +0200

    add dunk

---
+++ python_variable_annotations/9-element_length.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+"""
+    this module contains a function that returns values with types.
+"""
+
+
+from typing import Iterable, Sequence, Tuple
+
+def element_length(lst: Iterable[Sequence]) -> list[Tuple[Sequence, int]]:
+    """
+    Function that takes lst as argument. It is an iterable object with length.
+    Returns a list of tuples with a sequence and an int.
+    """
+    return [(i, len(i)) for i in lst]
