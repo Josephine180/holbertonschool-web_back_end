06289129cb6a3a75547205d439905a716678e263
Josephine180 <pierrejosephine09@gmail.com>
2025-04-28 14:07:24 +0200

    add task

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import asyncio
+import random
+
+
+"""
+Ce module contient une coroutine asynchrone `async_generator` qui génère 10
+nombres aléatoires entre 0 et 10, en attendant 1 seconde entre chaque nombre.
+La fonction `async_generator` utilise le module `random` pour générer les
+nombres
+et `asyncio` pour gérer l'asynchronisme.
+"""
+
+
+async def async_generator():
+    """
+    Cette coroutine génère 10 nombres aléatoires entre 0 et 10.
+
+    Elle attend 1 seconde avant de produire chaque nombre et le renvoie de
+    manière asynchrone avec `yield`.
+
+    Retour:
+        Un générateur asynchrone qui produit des nombres flottants
+        aléatoires entre 0 et 10.
+    """
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
