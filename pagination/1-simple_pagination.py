1a9bf222db3dd22201a5e0d893b410006a7bff2e
Josephine180 <pierrejosephine09@gmail.com>
2025-04-30 11:06:01 +0200

    pycodestyle

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -2,7 +2,7 @@
 import csv
 import math
 from typing import List
-from typing import Tuple
+
 
 
 """
@@ -41,12 +41,11 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        """ Return a page of the dataset """
-        assert (isinstance(page, int) and page > 0)
-        assert (isinstance(page_size, int) and page_size > 0)
-        start_index, end_index = index_range(page, page_size)
-        dataset = self.dataset()
-        if start_index >= len(dataset):
-        return []
-    return dataset[start_index:end_index]
-        
+            """ Return a page of the dataset """
+            assert (isinstance(page, int) and page > 0)
+            assert (isinstance(page_size, int) and page_size > 0)
+            start_index, end_index = index_range(page, page_size)
+            dataset = self.dataset()
+            if start_index >= len(dataset):
+                return []
+            return dataset[start_index:end_index]
